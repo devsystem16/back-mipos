e553d11255c84d8bb0ea594401c0390ba1a6d79d
mychael <desarrollos16@hotmail.com>
2020-06-14 06:22:40 -0500

    build

---
+++ public/precache-manifest.42265341932c255768689045cbeeb586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a73db8fc87e6fbe9ebe27ec05e501d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b7aea1ad4853c5d9bd",
+    "url": "/static/css/main.43cf41d9.chunk.css"
+  },
+  {
+    "revision": "bfac4f97fc1ce49483cc",
+    "url": "/static/js/2.fdb649e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fdb649e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b7aea1ad4853c5d9bd",
+    "url": "/static/js/main.890e340e.chunk.js"
+  },
+  {
+    "revision": "350eacb092db7dfdf393",
+    "url": "/static/js/runtime-main.1c96c7bf.js"
+  }
+]);
